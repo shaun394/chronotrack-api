713b6036eac33d355dc8964c40fb85c1d5a604be
Shaun <shaun@nexusit.dev>
2026-05-29 19:54:23 +0200

    chore: add dependency injection setup

---
+++ ChronoTrack.Application/DependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace ChronoTrack.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            Assembly assembly = typeof(DependencyInjection).Assembly;
+
+            services.AddMediatR(config =>
+            {
+                config.RegisterServicesFromAssembly(assembly);
+            });
+
+            services.AddValidatorsFromAssembly(assembly);
+
+            return services;
+        }
+    }
+}
