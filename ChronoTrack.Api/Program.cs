bc6774d53648f4bea69f499c39834af3141de9e2
Shaun <shaun@nexusit.dev>
2026-05-29 20:11:20 +0200

    chore: add database context setup

--- ChronoTrack.Api/Program.cs
+++ ChronoTrack.Api/Program.cs
@@ -13,7 +13,7 @@ namespace ChronoTrack.Api
             builder.Services.AddOpenApi();
 
             builder.Services.AddApplication();
-            builder.Services.AddInfrastructure();
+            builder.Services.AddInfrastructure(builder.Configuration);
 
             var app = builder.Build();
 
---
+++ ChronoTrack.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ChronoTrack.Infrastructure.Persistence
+{
+    public sealed class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
