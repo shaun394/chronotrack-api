4565c1a082bf0548ff90af9fbb7596e16678716b
Shaun <shaun@nexusit.dev>
2026-05-29 21:38:14 +0200

    feat: add backend foundation

---
+++ ChronoTrack.Api/Middleware/GlobalExceptionMiddleware.cs
@@ -0,0 +1,57 @@
+﻿using ChronoTrack.Api.Responses;
+using ChronoTrack.Application.Common.Exceptions;
+using ChronoTrack.Domain.Exceptions;
+using FluentValidation;
+using System.Net;
+
+namespace ChronoTrack.Api.Middleware
+{
+    public sealed class GlobalExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<GlobalExceptionMiddleware> _logger;
+
+        public GlobalExceptionMiddleware(
+            RequestDelegate next,
+            ILogger<GlobalExceptionMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(httpContext, ex);
+            }
+        }
+
+        private async Task HandleExceptionAsync(HttpContext httpContext, Exception ex)
+        {
+            HttpStatusCode statusCode = ex switch
+            {
+                ValidationException => HttpStatusCode.BadRequest,
+                DomainValidationException => HttpStatusCode.BadRequest,
+                NotFoundException => HttpStatusCode.NotFound,
+                _ => HttpStatusCode.InternalServerError
+            };
+
+            if (statusCode == HttpStatusCode.InternalServerError)
+            {
+                _logger.LogError(ex, "Unhandled exception occurred.");
+            }
+
+            httpContext.Response.StatusCode = (int)statusCode;
+            httpContext.Response.ContentType = "application/json";
+
+            var response = ApiResponse<object>.Fail(ex.Message);
+
+            await httpContext.Response.WriteAsJsonAsync(response);
+        }
+    }
+}
---
+++ ChronoTrack.Application/Common/Exceptions/NotFoundException.cs
@@ -0,0 +1,10 @@
+﻿namespace ChronoTrack.Application.Common.Exceptions
+{
+    public sealed class NotFoundException : Exception
+    {
+        public NotFoundException(string message)
+            : base(message)
+        {
+        }
+    }
+}
